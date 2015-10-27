8df95318358f293366f71cae21b6c74121253df6
CorrieASmith <corriesmith87@gmail.com>
2015-10-27 15:24:53 -0700

    add crud to exercise controller

--- app/controllers/exercises_controller.rb
+++ app/controllers/exercises_controller.rb
@@ -1,7 +1,50 @@
 class ExercisesController < ApplicationController
   def show
-    @target = Target.find(params[:question_id])
+    @target = Target.find(params[:target_id])
     @exercise = Exercise.find(params[:id])
     redirect_to target_path(@exercise.target)
+  end
+
+  def new
+    @target = Target.find(params[:target_id])
+    @exercise = @target.exercises.new
+  end
+
+  def create
+    @target = Target.find(params[:target_id])
+    @exercise = @target.exercises.new(exercise_params)
+    if @exercise.save
+      redirect_to target_path(@exercise.target)
+    else
+      render :new
+    end
+  end
+
+  def edit
+    @target = Target.find(params[:target_id])
+    @exercise = Exercise.find(params[:id])
+    render :edit
+  end
+
+  def update
+    @target = Target.find(params[:target_id])
+    @exercise = Exercise.find(params[:id])
+    if @exercise.update(exercise_params)
+      redirect_to target_path(@exercise.target)
+    else
+      render :edit
+    end
+  end
+
+  def destroy
+    @exercise = Exercise.find(params[:id])
+    target_id = params[:target_id]
+    @exercise.destroy
+    redirect_to target_path(target_id)
+  end
+
+  private
+  def exercise_params
+    params.require(:exercise).permit(:description, :chosen)
   end
 end
