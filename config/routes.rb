8abd6958f59f604e3142851d608ae757aa976eb6
CorrieASmith <corriesmith87@gmail.com>
2015-11-05 10:45:36 -0800

    routing bug fix; add exercise to navbar

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
   root :to => "home#index"
+  get '/users/:id', :to => 'users#show', :as => :user
   devise_for :users
   resources :targets do
     resources :exercises
