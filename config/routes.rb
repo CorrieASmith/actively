a9d4c5a53d472bfa1262b03dc93d125683dc4432
CorrieASmith <corriesmith87@gmail.com>
2015-11-06 12:41:12 -0800

    fix users controller routing issue

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,5 @@
 Rails.application.routes.draw do
   root :to => "home#index"
-  get '/users/:id', :to => 'users#show', :as => :user
   devise_for :users
   resources :targets do
     resources :exercises
