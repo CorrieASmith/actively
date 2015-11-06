76b10a065edf37108c1acc8b01761a90d04d8ace
CorrieASmith <corriesmith87@gmail.com>
2015-11-06 13:39:32 -0800

    add dinners controller

---
+++ app/controllers/dinners_controller.rb
@@ -0,0 +1,48 @@
+class DinnersController < ApplicationController
+  def index
+    @dinners = Dinner.all
+  end
+
+  def show
+    @dinner = Dinner.find(params[:id])
+  end
+
+  def new
+    @dinner = Dinner.new
+    render :new
+  end
+
+  def create
+    @dinner = Dinner.new(dinner_params)
+    if @dinner.save
+      flash[:notice] = "Your dinner was submitted successfully!"
+      redirect_to dinners_path
+    else
+      render :new
+    end
+  end
+
+  def edit
+    @dinner = Dinner.find(params[:id])
+    render :edit
+  end
+
+  def update
+    @dinner = Dinner.find(params[:id])
+    if @dinner.update(dinner_params)
+      redirect_to "/"
+    else
+      render :edit
+    end
+  end
+
+  def destroy
+    @dinner = Dinner.find(params[:id])
+    @dinner.destroy
+    redirect_to "/"
+  end
+
+  private def dinner_params
+    params.require(:dinner)
+  end
+end
