2ab95b796ac6bb5ab96c8279d05beff9c66365c4
CorrieASmith <corriesmith87@gmail.com>
2015-11-06 12:46:12 -0800

    fix users controller routing issue 2.0

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,10 @@
 Rails.application.routes.draw do
   root :to => "home#index"
-  devise_for :users
-  resources :targets do
+  devise_for :users do
+    get "/users/sign_out" => "devise/sessions#destroy", :as => :destroy_user_session
+  end
+
+  :targets do
     resources :exercises
   end
 end
